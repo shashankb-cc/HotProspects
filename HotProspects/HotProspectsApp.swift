ef58e8708bb5f9be7d557ac78d3a154578a11d77
shashankb-cc <shashank.b@codecraft.co.in>
2025-03-05 14:08:46 +0530

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Shashank B on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
